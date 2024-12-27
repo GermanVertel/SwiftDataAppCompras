bbe6b14adf52cd6ae402e6cb87f8432b0942d638
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-12-27 10:38:04 -0500

    nos preparamos para saltar a una siguiente vista, creamos la vista comprasview le agregamos el navigationLink y el navigationDestination a la vista contentview para cuando se de clic en la tarjeta se abra la nueva ventana con el nombre del titulo de la tarjeta que aparece en el contenview gracias a los parametros que recibe de @bindable

---
+++ SwiftDataAppCompras/Views/ComprasView.swift
@@ -0,0 +1,18 @@
+//
+//  ComprasView.swift
+//  SwiftDataAppCompras
+//
+//  Created by German David Vertel Narvaez on 27/12/24.
+//
+
+import SwiftUI
+
+struct ComprasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComprasView()
+}
