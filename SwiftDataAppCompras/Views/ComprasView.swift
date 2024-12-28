b1d3430cd81ac080e41593591f967f12a5433cfc
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-12-28 09:49:11 -0500

    se creo el diseño de la vista ComprasView donde ya se puede agragar los articulos en esta lista de comprar, agregar el presupuesto y el precio del articulo

--- SwiftDataAppCompras/Views/ComprasView.swift
+++ SwiftDataAppCompras/Views/ComprasView.swift
@@ -13,8 +13,45 @@ struct ComprasView: View {
     @Environment(\.modelContext) var context
     @Environment(\.dismiss) var dismiss
     
+    @State private var articulo = ""
+    @State private var precio = ""
+    @State private var cantidad = ""
+    
+    @FocusState private var isFocus: Bool
+    
     var body: some View {
-        Text(itemList.titulo)
+        VStack{
+            VStack{ // aqui va nuestro formulario
+                TextField("Articulo", text: $articulo)
+                    .textFieldStyle(.roundedBorder)
+                    .focused($isFocus)
+                
+                HStack{
+                    TextField("Precio", text: $precio)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 100)
+                    Spacer()
+                    //contador
+                }
+                
+                HStack{
+                    Button{
+                        //
+                    } label: {
+                        Text("Agregar")
+                    }
+                    Spacer()
+                        Text("Presupuesto: $\(itemList.presupuesto)")
+                }
+            }.padding(.all)
+            
+            List{
+                Section("Carrito"){
+                    }
+            }
+        }.navigationTitle(itemList.titulo)
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 
